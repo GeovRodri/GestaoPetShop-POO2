e0c78c15ab9708aea68a3ac78fa1ecc6fc30cbbd
Renato Pacheco <rpachecogeo@gmail.com>
2018-07-22 12:34:37 -0300

    Criando o ServicoFormValidator

---
+++ src/main/java/br/edu/ifg/validator/ServicoFormValidator.java
@@ -0,0 +1,29 @@
+package br.edu.ifg.validator;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import br.edu.ifg.form.ClienteFormDTO;
+
+@Controller
+public class ServicoFormValidator implements Validator{
+	
+	@Override
+	public boolean supports(Class<?> clazz) { 
+		return ServicoFormDTO.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "tipo", "required.tipo", "Preencha um tipo");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "valor", "required.valor", "Preencha um valor");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "descricao", "required.descricao", "Preencha um descricao");
+		
+		
+	}
+
+}
+
+
